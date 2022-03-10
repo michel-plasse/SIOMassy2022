60351f6aae8565ae241616241146d9217635e8a4
Michel Plasse <mplasse@free.fr>
2022-03-10 13:52:32 +0100

    Ajoute les scripts pour mettre en place la BD

---
+++ src/main/db/create-user.sql
@@ -0,0 +1,5 @@
+CREATE USER IF NOT EXISTS siomassy2022_user@localhost IDENTIFIED BY 'siomassy2022_pwd';
+GRANT ALL ON siomassy2022.* TO siomassy2022_user@localhost;
+GRANT EXECUTE ON siomassy2022.* TO siomassy2022_user@localhost;
+-- si l'instruction précédente échoue, lancer :
+-- GRANT SELECT ON mysql.proc TO siomassy2022_user@localhost;
