67ae8cdd3acae8a9eeed67639417dd8b1885d26f
Abdillahi <Abdillahi abdillahi.hassan@outlook.fr>
2022-04-02 16:13:56 +0200

    update class Inscription.java

--- src/main/java/modele/Inscription.java
+++ src/main/java/modele/Inscription.java
@@ -4,19 +4,126 @@
  */
 package modele;
 
+import java.util.Objects;
+
 /**
  *
  * @author Abdillahi
  */
 public class Inscription {
    private int id, id_groupe;
-   private String nom, prenom, psw, email, tel;
+   private String nom, prenom, mdp, email, tel;
    
-   public Inscription (int id, String nom, String prenom, String psw, String email, String tel){
+   public Inscription (int id, String nom, String prenom, String mdp, String email, String tel){
        this.nom = nom;
        this.prenom = prenom;
        this.email = email;
-       this.psw = psw;
+       this.mdp = mdp;
        this.tel = tel;
    }
+   public Inscription(String prenom, String nom, String email, String tel, String mdp) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.email = email;
+        this.tel = tel;
+        this.mdp = mdp;
+    }
+
+    public Inscription(int id, String prenom, String nom) {
+        this.id = id;
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+    public Inscription (){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setId_groupe(int id_groupe) {
+        this.id_groupe = id_groupe;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getId_groupe() {
+        return id_groupe;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Personne other = (Personne) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.prenom, other.prenom)) {
+            return false;
+        }
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.tel, other.tel)) {
+            return false;
+        }
+        if (!Objects.equals(this.pwd, other.pwd)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
