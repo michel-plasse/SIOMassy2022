28bcba053c02616094a753be6c0a5a1ecd6c75dc
Abdillahi <Abdillahi abdillahi.hassan@outlook.fr>
2022-04-05 10:11:38 +0200

    changer le nom de VUE-FORM

--- src/main/java/controleur/InscriptionServlet.java
+++ src/main/java/controleur/InscriptionServlet.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 public class InscriptionServlet extends HttpServlet {
 
     
-    private static final String VUE_FORM ="inscription.jsp";
+    private static final String VUE_FORM ="WEB-INF/inscription.jsp";
     
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
