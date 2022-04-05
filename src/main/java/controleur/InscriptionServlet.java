8422a24cfa30a884cb0010a4b3756cd526fc9e28
Abdillahi <Abdillahi abdillahi.hassan@outlook.fr>
2022-04-05 10:19:35 +0200

    importer dao et modele

--- src/main/java/controleur/InscriptionServlet.java
+++ src/main/java/controleur/InscriptionServlet.java
@@ -12,6 +12,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modele.Inscription;
+import modele.Personne;
+import dao.PersonneDao;
 
 /**
  *
