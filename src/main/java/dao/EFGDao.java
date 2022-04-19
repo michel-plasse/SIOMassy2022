00760b92fd84a3814f336b4bbaf13e8dc1a4054e
Karim Ammari <kalifein@gmail.com>
2022-04-19 15:39:18 +0200

    Ajoute la vue SQL v_efg_groupes_membres

--- src/main/java/dao/EFGDao.java
+++ src/main/java/dao/EFGDao.java
@@ -1,7 +1,5 @@
-
- /** Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template*/
- 
+/** Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template */
 package dao;
 
 import java.sql.Connection;
@@ -9,7 +7,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import modele.EFG;
+import modele.Groupe;
+import modele.Membre;
 import modele.Personne;
 
 /**
@@ -18,27 +20,42 @@ import modele.Personne;
  *
  */
 public class EFGDao {
-    
-  public static EFG getById(int idEFG) throws SQLException {
-    EFG result = null;
-    Connection connexion = Database.getConnection();
-    String sql = "SELECT * FROM efg WHERE id_efg = ?";
-    PreparedStatement stmt = connexion.prepareCall(sql);
-    stmt.setInt(1, idEFG);
-    ResultSet rs = stmt.executeQuery();
-    if (rs.next()) {
-      // d'abord le créateur en dur
-      Personne createur = new Personne(1, "Tryphon", "Tournesol");
-      result = new EFG(
-              idEFG, 
-              createur, 
-              rs.getInt("id_canal"),
-              rs.getString("intitule"),
-              rs.getTimestamp("cree_a").toLocalDateTime());
+
+    public static EFG getById(int idEFG) throws SQLException {
+        EFG result = null;
+        Connection connexion = Database.getConnection();
+        String sql = "SELECT * FROM v_efg_groupes_membres WHERE id_efg = ?";
+        PreparedStatement stmt = connexion.prepareCall(sql);
+        stmt.setInt(1, idEFG);
+        ResultSet rs = stmt.executeQuery();
+        boolean createurCree = false;
+        int idGroupe = 0;
+        Groupe groupe = null;
+        List<Groupe> groupes = new ArrayList<Groupe>();
+        while (rs.next()) {
+            Personne personne = new Personne(
+                    rs.getInt("id_personne"),
+                    rs.getString("prenom"),
+                    rs.getString("nom")
+            );
+            if (idGroupe != rs.getInt("id_groupe")) {
+                groupe = new Groupe(idEFG, rs.getInt("id_groupe"), null, new ArrayList<Personne>());
+                idGroupe = rs.getInt("id_groupe");
+                groupes.add(groupe);
+            }
+            groupe.getMembres().add(personne);
+        }
+        if (groupes.size() != 0) {
+            result = new EFG(
+                    idEFG,
+                    /*rs.getInt("id"), */
+                    null,
+                    1, //rs.getInt("id_canal"),
+                    groupes,
+                    "in titulé", //rs.getString("intitule"),
+                    LocalDateTime.now()); //rs.getTimestamp("cree_a").toLocalDateTime());
+        }
+        return result;
     }
-    return result;
-  }
-    
+
 }
-
-
