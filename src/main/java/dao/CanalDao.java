15375a605ca29c9ce2855a1f984d12ec60772638
Ilyesse HAMCHERIF <hamcherif.ilyesse@gmail.com>
2022-04-19 12:14:59 +0200

    Adding Canal DAO

--- src/main/java/dao/CanalDao.java
+++ src/main/java/dao/CanalDao.java
@@ -12,20 +12,21 @@ import java.sql.SQLException;
  */
 public class CanalDao {
 
-  public static Canal insert(int idCanal) throws SQLException {
-    Canal result = null;
-    Connection connection = Database.getConnection();
-    String sql = "INSERT INTO canal(id, nom) VALUES (?,?)";
-    PreparedStatement stmt = connection.prepareCall(sql);
-    stmt.setInt(1, idCanal);
-    ResultSet rs = stmt.executeQuery();
-    if (rs.next()) {
-      Canal createur = new Canal(23, "testavril");
-      result = new Canal(
+    public static Canal insert(int idCanal) throws SQLException {
+      Canal result = null;
+      Connection connection = Database.getConnection();
+      String sql = "INSERT INTO canal(id, nom) VALUES (?,?)";
+      PreparedStatement stmt = connection.prepareCall(sql);
+      stmt.setInt(1, idCanal);
+      ResultSet rs = stmt.executeQuery();
+      if (rs.next()) {
+          Canal createur = new Canal(4, "Test");
+          result = new Canal(
               rs.getInt("id_canal"),
-              rs.getString("nom"));
+              rs.getString("nom")
+          );
+      }
+      return result;
     }
-    return result;
-  }
 
 }
