b046c037f0f4c47dc94b77ca8ce80956e08df662
Michel Plasse <mplasse@free.fr>
2022-04-19 09:25:57 +0200

    Fait compiler reponsesSondage

--- src/main/java/controleur/ReponseSondageServlet.java
+++ src/main/java/controleur/ReponseSondageServlet.java
@@ -1,35 +1,21 @@
-
 package controleur;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet(name="ReponseSondageServlet", urlPatterns={"/reponseSondage"})
+@WebServlet(name = "ReponseSondageServlet", urlPatterns = {"/reponsesSondage"})
 public class ReponseSondageServlet extends HttpServlet {
- private static final String VUE= "WEB-INF/reponseSondage.jsp";  
 
+  private static final String VUE = "WEB-INF/reponsesSondage.jsp";
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
     request.getRequestDispatcher(VUE).forward(request, response);
-
-    } 
-
-
-
-   
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    }
+  }
 
 }
