93be49f4934f923a20bb9fb8743df160f6f18883
Ilyesse HAMCHERIF <hamcherif.ilyesse@gmail.com>
2022-05-03 18:55:20 +0200

    Create canal controller working

--- src/main/java/controleur/CanauxServlet.java
+++ src/main/java/controleur/CanauxServlet.java
@@ -1,17 +1,17 @@
 package controleur;
 
-import modele.Seance;
+import dao.CanalDao;
+import modele.Canal;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import modele.Canal;
 
 /**
  *
@@ -19,11 +19,33 @@ import modele.Canal;
  */
 @WebServlet(name = "CanauxServlet", urlPatterns = {"/canaux"})
 public class CanauxServlet extends HttpServlet {
-
-    private static final String VUE = "WEB-INF/canaux.jsp";
+  private String VUE_CANAUX = "WEB-INF/canaux.jsp";
 
     /**
-     * Handles the HTTP <code>GET</code> method.
+    * Handles the HTTP <code>GET</code> method.
+    *
+    * @param request servlet request
+    * @param response servlet response
+    * @throws ServletException if a servlet-specific error occurs
+    * @throws IOException if an I/O error occurs
+    */
+   @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int memberId = 1;
+        List<Canal> canaux = null;
+        try {
+            canaux = CanalDao.getAll();
+        }
+          catch (SQLException ex) {
+            Logger.getLogger(CanauxServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        request.setAttribute("canaux", canaux);
+        request.getRequestDispatcher(VUE_CANAUX).forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
      * @param response servlet response
@@ -31,14 +53,24 @@ public class CanauxServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        // D'abord en dur (il faudra chercher les données via la Dao)
-        List<Canal> canaux = new ArrayList<>();
-        canaux.add(new Canal(1, "SIO 2021 projet"));
-        canaux.add(new Canal(2, "SIO 2021 java"));
-        canaux.add(new Canal(3, "CDA 202"));
-        request.setAttribute("canaux", canaux);
-        request.getRequestDispatcher(VUE).forward(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String nom = request.getParameter("nom");
+        try {
+            CanalDao.insert(new Canal(1,  nom));
+        }
+        catch (SQLException ex) {
+        }
+        response.sendRedirect("mes-canaux");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
     }
 }
