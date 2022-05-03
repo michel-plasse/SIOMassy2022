af58285b998904566f23b61b2a19801ee858dffc
Ilyesse HAMCHERIF <hamcherif.ilyesse@gmail.com>
2022-05-03 14:53:54 +0200

    Canaux Model - Update

--- src/main/java/modele/Canal.java
+++ src/main/java/modele/Canal.java
@@ -1,36 +1,17 @@
 package modele;
 
-import java.util.Objects;
-
-/**
- *
- * @author hamcherif.ilyesse@gmail.com
- */
 public class Canal {
-
+    
+    /**
+    *
+    * @author hamcherif.ilyesse@gmail.com
+    */
     private int id;
     private String nom;
 
-    /**
-     * Constructeur
-     *
-     * @param id
-     * @param nom
-     */
     public Canal(int id, String nom) {
         this.id = id;
         this.nom = nom;
-    }
-
-    public Canal(String nom) {
-        this.nom = nom;
-    }
-
-    public Canal(int id) {
-        this.id = id;
-    }
-
-    public Canal() {
     }
 
     public int getId() {
@@ -48,26 +29,4 @@ public class Canal {
     public void setNom(String nom) {
         this.nom = nom;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Canal other = (Canal) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (!Objects.equals(this.nom, other.nom)) {
-            return false;
-        }
-        return true;
-    }
-
 }
